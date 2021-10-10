944e45c45ceeb6a58b7b4f2ac90fad84f8367255
zeratulspc <zeratulspc@gmail.com>
2021-10-10 21:43:10 +0900

    Event handle 코드 추가

--- index.js
+++ index.js
@@ -9,38 +9,18 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
-	console.log('Server ON');
-});
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const { commandName } = interaction;
-
-    const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-});
-
-client.on('messageCreate', (message) => {
-    if(message.content === '울랄라~') {
-        message.reply({
-            content: '울랄라~~♬'
-        });
-    }
-});
+}
 
 client.login(token);
