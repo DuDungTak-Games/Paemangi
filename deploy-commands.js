1cd9cdc427a0fa4ff907da3b1e69dd21ab4b7990
zeratulspc <zeratulspc@gmail.com>
2021-10-10 21:37:32 +0900

    명령어 동기화 코드 추가

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,15 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [
-	new SlashCommandBuilder().setName('패망아춤춰').setDescription('패망이가 춤을 춥니다'),
-	new SlashCommandBuilder().setName('울랄라').setDescription('울랄라~'),
-]
-	.map(command => command.toJSON());
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9' }).setToken(token);
 
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Server ON');
 });
 
 client.on('interactionCreate', async interaction => {
@@ -12,12 +23,24 @@ client.on('interactionCreate', async interaction => {
 
 	const { commandName } = interaction;
 
-	if (commandName === '패망아춤춰') {
-		await interaction.reply('둠칫둠칫');
-	} else if (commandName === '울랄라') {
-		await interaction.reply('울랄라~♬');
+    const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
+client.on('messageCreate', (message) => {
+    if(message.content === '울랄라~') {
+        message.reply({
+            content: '울랄라~~♬'
+        });
+    }
+});
 
 client.login(token);
