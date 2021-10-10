7cf7229e395d353d62ab9074158e7c34a92f15f6
zeratulspc <zeratulspc@gmail.com>
2021-10-10 22:38:34 +0900

    명렁어 Global로 변경

--- index.js
+++ index.js
@@ -12,15 +12,23 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
-}
+});
 
 client.login(token);
